a6496a06d9bd8abf7dd47d4d335eb2606ffc3721
diegoalvarezg <554716@unizar.es>
2018-03-20 03:56:13 +0100

    Modified client part. Twitter Streaming working

--- src/main/resources/static/functions.js
+++ src/main/resources/static/functions.js
@@ -1,6 +1,21 @@
+var subscription = null
+
 function registerTemplate() {
     template = $("#template").html();
     Mustache.parse(template);
+}
+
+function connect() {
+    var sock = new SockJS('/twitter');
+    client = Stomp.over(sock);
+    client.connect({}, function(state) {
+        console.log('State ' + state);
+    });
+}
+
+function addTweet(tweet) {
+    $("#resultsBlock").append(Mustache.render(template, JSON.parse(tweet.body)));
+    console.log("Tweet added")
 }
 
 function registerSearch() {
@@ -8,17 +23,24 @@ function registerSearch() {
         event.preventDefault();
         var target = $(this).attr('action');
         var query = $("#q").val();
-        $.get(target, { q: query } )
-            .done( function(data) {
-                $("#resultsBlock").empty().append(Mustache.render(template, data));
-            }).fail(function() {
+
+        if (subscription != null) {
             $("#resultsBlock").empty();
-        });
+            subscription.unsubscribe();
+            console.log("Unsubscription")
+
+        }
+
+        client.send("/app/" + target, {}, query);
+        subscription = client.subscribe('/queue/search/' + query, addTweet);
+        console.log("Client subscribed");
+
     });
 }
 
 $(document).ready(function() {
-    registerTemplate()
+    registerTemplate();
+    connect();
 	registerSearch();
 });
 
